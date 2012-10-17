245280f214049dc9b932189f198a07005e799c4b
watcha <paully-24@hotmail.com>
2012-10-17 21:59:28 +0700

    add for loop

--- test/src/simple/T.java
+++ test/src/simple/T.java
@@ -2,6 +2,8 @@ package simple;
 
 public class T {
 	public static void main(String args[]) {
-		System.out.println("hello");
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("hello");
+		}
 	}
 }
