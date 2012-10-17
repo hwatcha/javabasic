61cbf0c6719b22f0cc37b9d65c252b25f0dcfdc6
watcha <paully-24@hotmail.com>
2012-10-17 22:10:20 +0700

    Add class A

---
+++ test/src/simple/A.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class A {
+
+	public A() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	public void showMsg() {
+		System.out.println("A");
+	}
+}
--- test/src/simple/T.java
+++ test/src/simple/T.java
@@ -2,6 +2,8 @@ package simple;
 
 public class T {
 	public static void main(String args[]) {
+		A a = new simple.A();
+		a.showMsg();
 		for (int i = 0; i <= 10; i++) {
 			if (i % 2 == 0) {
 				System.out.print("*");
