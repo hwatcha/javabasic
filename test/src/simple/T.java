4a8d74d1d3e489b9f3e64d683ded0413de9d19c3
watcha <paully-24@hotmail.com>
2012-10-17 22:15:49 +0700

    Update test/src/simple/T.java
    test

--- test/src/simple/T.java
+++ test/src/simple/T.java
@@ -6,9 +6,9 @@ public class T {
 		a.showMsg();
 		for (int i = 0; i <= 10; i++) {
 			if (i % 2 == 0) {
-				System.out.print("*");
+				System.out.print("*1");
 			}else {
-				System.out.print("-");
+				System.out.print("2-");
 			}
 		}
 	}
