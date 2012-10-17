d1c494dbc1c9266af5c4b0d496ac49618b675469
watcha <paully-24@hotmail.com>
2012-10-17 22:03:20 +0700

    Changed logic

--- test/src/simple/T.java
+++ test/src/simple/T.java
@@ -3,7 +3,11 @@ package simple;
 public class T {
 	public static void main(String args[]) {
 		for (int i = 0; i <= 10; i++) {
-			System.out.println("hello");
+			if (i % 2 == 0) {
+				System.out.print("*");
+			}else {
+				System.out.print("-");
+			}
 		}
 	}
 }
